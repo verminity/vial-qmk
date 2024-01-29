86ea2c32ae447d67529b7a98e07152e3f9a4b374
Martin Harris <verminity@gmail.com>
2024-01-29 09:57:49 +0000

    Added personal custom config

---
+++ keyboards/crkbd/keymaps/martinharris/rules.mk
@@ -0,0 +1,17 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+MOUSEKEY_ENABLE     = no
+OLED_ENABLE         = no
+OLED_DRIVER         = SSD1306
+EXTRAKEY_ENABLE     = yes
+COMBO_ENABLE        = no
+
+QMK_SETTINGS        = yes
+
+CAPS_WORD_ENABLE    = yes
+
+VIALRGB_ENABLE      = no
